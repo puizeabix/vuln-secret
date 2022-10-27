f1dd1e6ff0029708e6c2b37062ea586ac63f4c1a
Prawit Chaivong <prawit.c@zeabix.com>
2022-10-28 00:32:12 +0700

    Add code

---
+++ code.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	conn := "mongodb://dbUserAdmin:crt5S7g2G6srTkJm@mongodb-service.app.svc.cluster.local:27017/apec_db"
+	fmt.Println("Start service....")
+}	
